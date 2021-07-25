c431ca02508af4556f2811a0d61ea55fbfb11abb
vivekpaliwal-87 <87900401+vivekpaliwal-87@users.noreply.github.com>
2021-07-25 15:22:31 +0530

    Update HelloClass.java

--- src/main/java/test/HelloClass.java
+++ src/main/java/test/HelloClass.java
@@ -4,6 +4,6 @@ public class HelloClass {
     public static void main (String args[]) {
         System.out.println("Hello Maven Project...");
         System.out.println("Hello Maven Project...");
-        System.out.println("Hello Maven Project...11");
+        System.out.println("Hello Maven Project...1122");
     }
 }
